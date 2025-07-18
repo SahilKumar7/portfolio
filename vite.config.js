ff6410944fe37f421e07f76efa8f24488adf5f6b
Sahil <55492780+SahilKumar7@users.noreply.github.com>
2025-07-18 15:48:00 +0530

    chore: update build config, add robots.txt and sitemap

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
-  // server: {
-  //   watch: {
-  //     usePolling: true
-  //   }
-  // }
+  base: process.env.VITE_BASE || "/portfolio",
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
